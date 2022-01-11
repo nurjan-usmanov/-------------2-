80fddd6b54d3c5aaca97a09018d44d897c56a8d1
nurjan-usmanov <nurjanuk@gmail.com>
2022-01-12 00:50:00 +0400

    Nurjanuk

---
+++ app.js
@@ -0,0 +1,13 @@
+// function my() {
+//     let a = document.getElementById('move');
+//     a.style.left = '10px'
+// }
+
+
+function v() {
+    let sum = 0;
+    return sum += 5
+}
+
+console.log(v());
+console.log(v());
